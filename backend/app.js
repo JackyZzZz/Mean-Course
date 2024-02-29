49f25921e9ce08e4c0c4b49f71af8c070157f59c
Jun Xia <xiaj8@uci.edu>
2024-02-28 23:30:25 -0800

    Deleting Documents

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://JackyZzZ:20021024Han-@cluster0.oudqqsl.mongodb.net/node-angular?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        console.log('Connected to database!');
+    })
+    .catch(() => {
+        console.log('Connection failed!');
+    });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,27 +35,26 @@ app.post("/api/posts", (req, res, next) => {
         title: req.body.title,
         content: req.body.content
     });
-    console.log(post);
+    post.save();
     res.status(201).json({
         message: 'Post added successfully'
     });
 });
 
 app.get('/api/posts', (req, res, next) => {
-    const posts = [
-        { id: 'fadf12421l', 
-        title: 'First server-side post', 
-        content: 'This is coming from the server' 
-        },
-        {
-            id: 'ksajflaj132',
-            title: 'Second server-side post',
-            content: 'This is coming from the server!'
-        }
-    ];
-    res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: posts
+    Post.find()
+        .then(documents => {
+            res.status(200).json({
+                message: 'Posts fetched successfully!',
+                posts: documents
+            });
+        });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+    Post.deleteOne({_id: req.params.id}).then(result => {
+        console.log(result);
+        res.status(200).json({ message: 'Post deleted!' });
     });
 });
 
