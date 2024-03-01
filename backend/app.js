43840f26bb604930396f2802c68d42f5ebe6385e
Jun Xia <xiaj8@uci.edu>
2024-02-29 22:59:46 -0800

    Fixed some bugs

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,6 @@ app.use((req, res, next) => {
     res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
     );
     next();
 });
