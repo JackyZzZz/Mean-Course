e2776d1b69941dff50eecbd100b200936e86cc0c
Jun Xia <xiaj8@uci.edu>
2024-03-01 14:22:50 -0800

    Finishing the Image Validator

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
+
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html',
@@ -26,7 +28,10 @@ export class PostCreateComponent implements OnInit {
             title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]
             }),
             content: new FormControl(null, {validators: [Validators.required]}),
-            image: new FormControl(null, {validators: [Validators.required]})
+            image: new FormControl(null, {
+                validators: [Validators.required],
+                asyncValidators: [mimeType]
+            })
         });
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
             if (paramMap.has('postId')) {
